a7ab79790645a5e3c358f261163ced9e877a457c
sophiaecl <67115618+sophiaecl@users.noreply.github.com>
2025-02-19 23:36:05 +0100

    changing database references

--- app/api/auth.py
+++ app/api/auth.py
@@ -3,6 +3,7 @@ from starlette.requests import Request
 from starlette.responses import RedirectResponse
 from passlib.context import CryptContext
 from google.cloud import bigquery
+from typing import Optional
 import os
 import uuid
 from dotenv import load_dotenv
@@ -11,6 +12,7 @@ load_dotenv()
 
 USER_DATASET_ID = os.getenv("USER_DATASET_ID")
 USERNAME_TABLE_ID = os.getenv("USERNAME_TABLE_ID")
+USER_INFO_TABLE_ID = os.getenv("USER_INFO_TABLE_ID")
 
 router = APIRouter()
 
@@ -18,13 +20,16 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 client = bigquery.Client()
 dataset_id = USER_DATASET_ID
-table_id = USERNAME_TABLE_ID
-table_ref = client.dataset(dataset_id).table(table_id)
+user_table_id = USERNAME_TABLE_ID
+user_table_ref = client.dataset(dataset_id).table(user_table_id)
+
+user_info_id = USER_INFO_TABLE_ID
+user_info_ref = client.dataset(dataset_id).table(user_info_id)
 
 # User Registration Endpoint
 @router.post("/register")
-async def register(username: str = Form(...), password: str = Form(...)):
-    query = f"SELECT username FROM `{dataset_id}.{table_id}` WHERE username = @username"
+async def register(username: str = Form(...), password: str = Form(...), name: str = Form(...), age: int = Form(...), gender: Optional[str] = Form(None)):
+    query = f"SELECT username FROM `{dataset_id}.{user_table_id}` WHERE username = @username"
     job_config = bigquery.QueryJobConfig(
         query_parameters=[
             bigquery.ScalarQueryParameter("username", "STRING", username)
@@ -39,19 +44,30 @@ async def register(username: str = Form(...), password: str = Form(...)):
     hashed_password = pwd_context.hash(password)
     user_id = str(uuid.uuid4())
 
-    rows_to_insert = [
+    user_rows = [
         {u"id": user_id, u"username": username, u"password": hashed_password}
     ]
-    errors = client.insert_rows_json(table_ref, rows_to_insert)
-    if errors:
-        raise HTTPException(status_code=500, detail=str(errors))
+
+    user_errors = client.insert_rows_json(user_table_ref, user_rows)
+
+    if user_errors:
+        raise HTTPException(status_code=500, detail=str(user_errors))
+
+    info_rows = [
+        {u"user_id": user_id, u"name": name, u"age": age, u"gender": gender}
+    ]
+
+    info_errors = client.insert_rows_json(user_info_ref, info_rows)
+    
+    if info_errors:
+        raise HTTPException(status_code=500, detail=str(info_errors))
 
     return {"message": "User registered successfully"}
 
 # User Login Endpoint
 @router.post("/login")
 async def login(request: Request, username: str = Form(...), password: str = Form(...)):
-    query = f"SELECT id, username, password FROM `{dataset_id}.{table_id}` WHERE username = @username"
+    query = f"SELECT id, username, password FROM `{dataset_id}.{user_table_id}` WHERE username = @username"
     job_config = bigquery.QueryJobConfig(
         query_parameters=[
             bigquery.ScalarQueryParameter("username", "STRING", username)
